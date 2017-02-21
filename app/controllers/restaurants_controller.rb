f15ac5a6bedf0ef977d198bce8622a7bc0e5d639
Thomas William Page <tom.w.page@gmail.com>
2017-02-21 23:06:49 +0100

    really basic mvc structure with three basic tests

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,19 @@
 class RestaurantsController < ApplicationController
   def index
-    # raise 'hello from the index action in the restaurant controller'
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+private
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
   end
 end
--- spec/restaurant_feature_spec.rb
+++ spec/restaurant_feature_spec.rb
@@ -1,6 +1,19 @@
 require 'rails_helper'
 
+
 feature 'restaurants' do
+
+  context 'Checking the backend' do
+    before do
+      Restaurant.create(name: 'KFC')
+    end
+    scenario 'should have added a restaurant to the db' do
+      visit '/restaurants'
+      expect(page).to have_content 'KFC'
+      expect(page).not_to have_content 'No restaurants yet'
+    end
+  end
+
   context 'no restaurants should have been added' do
     scenario 'should display a prompt to add a restaurant' do
       visit '/restaurants'
@@ -8,4 +21,15 @@ feature 'restaurants' do
       expect(page).to have_link 'Add a restaurant'
     end
   end
+
+  context 'using the add a restaurant button' do
+    scenario 'should be able to add a restaurant on the main page' do
+      visit '/restaurants'
+      click_link 'Add a restaurant'
+      fill_in 'Name', with: 'KFC'
+      click_button 'Create Restaurant'
+      expect(page).to have_content 'KFC'
+    end
+  end
+
 end

8fc8bb25e7af047cedb4593cd73a2c4cc1847323
Thomas William Page <tom.w.page@gmail.com>
2017-02-22 18:28:47 +0100

    CRUD works

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -12,8 +12,29 @@ class RestaurantsController < ApplicationController
     redirect_to '/restaurants'
   end
 
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    flash[:notice] = 'Restaurant succefully DESTORYED'
+    redirect_to '/restaurants'
+  end
+
 private
   def restaurant_params
-    params.require(:restaurant).permit(:name)
+    params.require(:restaurant).permit(:name, :description)
   end
 end
--- spec/restaurant_feature_spec.rb
+++ spec/restaurant_feature_spec.rb
@@ -27,8 +27,66 @@ feature 'restaurants' do
       visit '/restaurants'
       click_link 'Add a restaurant'
       fill_in 'Name', with: 'KFC'
+      fill_in 'Description', with: 'Shitty Licky Chicken'
       click_button 'Create Restaurant'
       expect(page).to have_content 'KFC'
+    end
+  end
+
+  context 'using the back button' do
+    scenario 'clicking the back button should take you back' do
+      visit '/restaurants'
+      click_link 'Add a restaurant'
+      click_link 'Back'
+      expect(current_path).to eq '/restaurants'
+    end
+  end
+
+  context 'clicking the link to the restaurant' do
+
+    let!(:kfc){ Restaurant.create(name: 'KFC', description: 'Shitty Licky Chicken') }
+
+    scenario 'should take you to the specific restaurant page' do
+      visit '/restaurants'
+      click_link 'KFC'
+      expect(page).to have_content 'KFC'
+      expect(page).to have_content 'Shitty Licky Chicken'
+      expect(current_path).to eq "/restaurants/#{kfc.id}"
+    end
+
+    scenario 'check back button' do
+      visit '/restaurants'
+      click_link 'KFC'
+      click_link 'Back'
+      expect(current_path).to eq '/restaurants'
+    end
+  end
+
+  context 'checking the update function' do
+
+    let!(:kfc){ Restaurant.create(name: 'KFC', description: 'Shitty Licky Chicken') }
+
+    scenario 'should be able to go back and alter data' do
+      visit '/restaurants'
+      click_link 'KFC'
+      click_link 'Edit KFC'
+      fill_in 'Name', with: 'Boom'
+      fill_in 'Description', with: 'Delish'
+      click_button 'Update Restaurant'
+      expect(current_path).to eq '/restaurants'
+      expect(page).to have_content 'Boom'
+    end
+  end
+
+  context 'checking the delete/destroy method works' do
+     before { Restaurant.create name: 'KFC', description: 'Deep fried goodness' }
+    scenario 'should be able to delete destory a restaurant' do
+      visit '/restaurants'
+      click_link 'KFC'
+      click_link 'Delete KFC'
+      expect(current_path).to eq '/restaurants'
+      expect(page).not_to have_content 'KFC'
+      expect(page).to have_content 'No restaurants yet'
     end
   end
 

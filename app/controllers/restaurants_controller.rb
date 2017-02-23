e32dec0e0026b328a6d305875076314dfd2e2524
Thomas William Page <tom.w.page@gmail.com>
2017-02-23 23:51:35 +0100

    still trying to work out putting review on rest show

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -14,6 +14,7 @@ class RestaurantsController < ApplicationController
 
   def show
     @restaurant = Restaurant.find(params[:id])
+    @review = @restaurant.reviews
   end
 
   def edit
---
+++ spec/review_feature_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+feature 'reviews' do
+
+  context 'checking the review function' do
+
+    let!(:kfc){ Restaurant.create(name: 'KFC', description: 'Shitty Licky Chicken') }
+
+    scenario 'you should be able to leave a review for a restaurant' do
+      visit '/restaurants'
+      click_link 'Review this restaurant'
+      fill_in 'Thoughts', with: 'Pretty shitty chicken'
+      select '3', from: 'Rating'
+      click_button 'Leave Review'
+
+      expect(page).to have_content 'Pretty shitty chicken'
+      expect(current_path).to eq '/restaurants'
+    end
+  end
+
+end
